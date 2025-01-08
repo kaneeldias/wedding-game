3ce32104e0ec02ae6783e4c95b746841aae61204
kaneeldias <kaneeldias@hotmail.com>
2025-01-08 11:56:34 +0530

    Initial commit

---
+++ src/fonts.ts
@@ -0,0 +1,11 @@
+import {Alex_Brush, Roboto} from "next/font/google";
+
+export const alexBrush = Alex_Brush({
+    subsets: ["latin"],
+    weight: "400",
+});
+
+export const roboto = Roboto({
+    subsets: ["latin"],
+    weight: "400",
+});
