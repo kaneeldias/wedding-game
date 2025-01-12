0e8db6a9c63e2ba5f51574bc5fff558f27446d75
kaneeldias <kaneeldias@hotmail.com>
2025-01-12 12:47:48 +0530

    Add leaderboard to points bar

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,5 +1,6 @@
 import {Card, Table, TableData} from "@mantine/core";
 import {getPoints} from "@/utils/points-utils";
+import PointsBar from "@/app/components/PointsBar";
 
 export default async function Leaderboard() {
     const points = await getPoints();
@@ -10,7 +11,7 @@ export default async function Leaderboard() {
     
     return (
         <div className={`flex flex-col p-5 w-full space-y-5 items-start h-full`}>
-            {/*<PointsBar/>*/}
+            <PointsBar/>
             <Card shadow="sm" padding="xs" radius="md" withBorder className={`w-full flex flex-row`}>
                 <div className={`font-bold px-2 text-lg`}>Leaderboard</div>
                 <Table data={tableData}/>
